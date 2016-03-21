72985fb0de3d4c7b7cdf2247cfd9d4632c318b32
Jorge-Perez345 <jorge.perez@ms223.org>
2016-03-21 08:21:42 -0400

    Delete March21DoNow.py

--- March21DoNow.py
+++
@@ -1,6 +0,0 @@
-import random
-random.randint(0, 3) #randint 
-random.randint(0, 3) #the values 0 and 3 serve as the range
-print(random.randint(0, 3))
-print(random.randint(0, 3))
-print(random.randint(0, 3))
