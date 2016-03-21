76b72c590982cad0b50df114e0ad4fd3bc4fc8f9
genesishiciano <genesis.hiciano@ms223.org>
2016-03-21 08:42:18 -0400

    Create Find_odds.py

---
+++ Find_odds.py
@@ -0,0 +1,13 @@
+list= [3,4,7,13,54,32,653,256,1,41,65,83,92,31]
+
+def find_odds(input):
+    for x in input:
+        if x%2== 1:
+            print x
+
+find_odds(list)
+
+list2=[2,4,5,3,7,3,89]
+find_odds(list2)
+
+def odd_
