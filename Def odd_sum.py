c7d1e9dd79635d662e397e3c2f20d98887b1177e
genesishiciano <genesis.hiciano@ms223.org>
2016-03-22 08:46:18 -0400

    Update Def odd_sum.py

--- Def odd_sum.py
+++ Def odd_sum.py
@@ -1,6 +1,6 @@
-def odd_sum(input):
-    total= 0
+def odd_sum(input): # declares the function, the purpose of the function is to add all of the odd numbers
+    total= 0 # create a variable with the value of zero
     for x in input:
-        if x%2==1:
-            total= total+ x
-    print total
+        if x%2==1: # if the number has a remainder of one, then it is considered an odd number. 
+            total= total+ x # add the variable "total" with the outputs of "odd_sum"
+    print total # print the sum
