e2b1f0b7ea7f86c42d0f415bce85f3cca6669dd2
genesishiciano <genesis.hiciano@ms223.org>
2016-03-29 08:36:11 -0400

    Create Countdown.py

---
+++ Countdown.py
@@ -0,0 +1,5 @@
+n= input("give me a number") # ask the user for a number, the number that the user puts in is going to be the highest value.
+
+while n>0 : # if the number is greater than 0, keep looping the code. 
+    print n # print out the value of n,while it is being looped
+    n=n-1 # subtract the value of n, so it will go down to 0.
