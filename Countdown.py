331574a5feb05b1a83fae034f485d74102d0b6f9
bensamuelskalow@gmail.com <bensamuelskalow@gmail.com>
2016-03-29 08:42:13 -0400

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -3,5 +3,6 @@ n= input("give me a number") # ask the user for a number, the number that the us
 while n>0 : # if the number is greater than 0, keep looping the code. 
     print n # print out the value of n,while it is being looped
     n=n-1 # subtract the value of n, so it will go down to 0.
-if n==0: # if the value for N equals 0
-        print "boom!" # print out "BOOM!"
+print "boom!" # print out "BOOM!"
+
+# I got rid of the conditional...you don't actually need it, you can just print "boom" after the loop is done!
