d601c1929b60a678ebaa006130fbf9ccb3e675fd
bensamuelskalow@gmail.com <bensamuelskalow@gmail.com>
2016-03-29 08:43:01 -0400

    Update Countdown.py
    I got rid of the conditional...you don't actually need it, you can just print "boom" after the loop is done!

--- Countdown.py
+++ Countdown.py
@@ -5,4 +5,3 @@ while n>0 : # if the number is greater than 0, keep looping the code.
     n=n-1 # subtract the value of n, so it will go down to 0.
 print "boom!" # print out "BOOM!"
 
-# I got rid of the conditional...you don't actually need it, you can just print "boom" after the loop is done!
