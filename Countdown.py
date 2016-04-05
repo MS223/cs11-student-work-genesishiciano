ee1c20b09344428ca8f84e91d2a8efdd14565532
genesishiciano <genesis.hiciano@ms223.org>
2016-04-05 08:37:35 -0400

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -3,11 +3,13 @@
 # 2. numbers== is the starting point on the guessing game
 #3.guess== the number the person guesses
 # name== asking the person their name
+# tries== how many tries it took to get the answer
 
 
 name=raw_input(" What's your name?")
 Upper_bound =input("What is the highest value you want to use?") #I switched from 'raw_input'
 #to 'input '. raw input gives me a string not a integer.
+tries= 0
 import random
 guess= input(" what number do you think it is?")
 answer=random.randint(0,Upper_bound)
@@ -15,7 +17,9 @@ print answer
 while  guess != answer:
     if guess>answer:
         print "too high"
+        tries= tries+1
     elif guess<answer:
         print"too low"
     guess= input(" what number do you think it is?")
-print name,"You got it right! it's ",answer
+    print name,"it took you",tries,"try."
+print name +"!","You got it right! it's",answer
