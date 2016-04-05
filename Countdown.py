c437556e9d541c942a62e0107ecc3ebfa883c791
genesishiciano <genesis.hiciano@ms223.org>
2016-04-05 08:19:34 -0400

    Update Countdown.py

--- Countdown.py
+++ Countdown.py
@@ -1,7 +1,21 @@
-n= input("give me a number") # ask the user for a number, the number that the user puts in is going to be the highest value.
+# variables
+# 1. q == the highest value the user wants to use
+# 2. numbers== is the starting point on the guessing game
+#3.guess== the number the person guesses
 
-while n>0 : # if the number is greater than 0, keep looping the code. 
-    print n # print out the value of n,while it is being looped
-    n=n-1 # subtract the value of n, so it will go down to 0.
-print "boom!" # print out "BOOM!"
 
+
+
+Upper_bound =input("What is the highest value you want to use?") #I switched from 'raw_input'
+#to 'input '. raw input gives me a string not a integer.
+import random
+guess= input(" what number do you think it is?")
+answer=random.randint(0,Upper_bound)
+print answer
+while  guess != answer:
+    if guess>answer:
+        print "too high"
+    elif guess<answer:
+        print"too low"
+    guess= input(" what number do you think it is?")
+print "You got it right! it's ",answer
