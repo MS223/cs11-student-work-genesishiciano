6c8fe26f829e189267e609bdb1732031e0eb3ce2
genesishiciano <genesis.hiciano@ms223.org>
2016-04-12 08:08:45 -0400

    Update April11_GuidedPractice.py

--- April11_GuidedPractice.py
+++ April11_GuidedPractice.py
@@ -2,18 +2,22 @@
 def print_only(x):
    y = x * 2
    print y
-
+""" This function shows  the value of 'x' multiplied by two"""
 # how is this one different ?
 def return_only(x):
    y = x * 2
    return y
+   """ This Function shows the value for 'x' only to the computer""" 
 
 # let's try to use our 2 functions
 print "running print_only ..."
 print_only(7)
+""" It prints out the final product of 7*2-which should be 14"""
 
 print "running return_only ..."
 return_only(7)
+ """ This function does the same thing as the previous one- though, 
+ the only difference is that the computer doesn't let the user see it"""
 
 print "printing print_only ..."
 print print_only(7)
