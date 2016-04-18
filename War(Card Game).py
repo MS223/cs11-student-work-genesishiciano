57f0ace5127f295ee3376eb8d782479b46f0431e
genesishiciano <genesis.hiciano@ms223.org>
2016-04-18 08:01:26 -0400

    Update War(Card Game).py

--- War(Card Game).py
+++ War(Card Game).py
@@ -6,3 +6,11 @@ print player_turn()
 cards=range(2,15)*4
 random.shuffle(cards)
 print cards
+def Compare_score(card1, card2):
+    if card1 >card2:
+        print player1
+    elif card1<card2:
+        print player2
+    elif card1==card2:
+        print "its a tie!"
+compare_score()
