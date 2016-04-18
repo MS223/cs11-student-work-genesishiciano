5a493bbf2fe9a2fce98fae9ad49530085be6f289
genesishiciano <genesis.hiciano@ms223.org>
2016-04-18 08:42:42 -0400

    Create Practice_Makes_Perfect.py

---
+++ Practice_Makes_Perfect.py
@@ -0,0 +1,20 @@
+def is_even(x):
+    if x % 2== 0:
+        return True
+    else:
+        return False
+        print is_even(4)
+true 
+print is_even(7)
+false 
+def is_int(x):
+    if x ==int(x):
+        return True
+    else:
+        return False
+
+n= 1234
+print n
+z= str(n)
+print z[3]
+
