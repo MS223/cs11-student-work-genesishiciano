3bb3dc7ea2565ed4f94fb173b92501ee4e678aa3
genesishiciano <genesis.hiciano@ms223.org>
2016-05-16 08:40:07 -0400

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,16 @@
+dictionary_lab= {
+'name':'Genesis Hiciano',
+'School':'The laboratory of Finanace and Technology',
+'Email': 'Genesis.Hiciano@ms223.org',
+'Username':'Genesis Hiciano',
+'Where am I From':'Dominican Republic',
+'Number Of Siblings':'2 siblings',
+ 'Skills':'I like to be quiet.',
+ 'About Me': 'I like to be quiet-as stated previously.',
+'Favorite quote':'BLANK.',
+'Favorite Movie': 'ANNIE',
+ 'Favorite TV Show': 'The Wonder years ',
+'Favorite Book': 'I do not have one'
+}
+print dictionary_lab
+print dictionary_lab.get('Favorite Movie')
