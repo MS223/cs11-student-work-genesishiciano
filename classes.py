cd48a938551bb71f2469fb36fdf75fec011808d4
genesishiciano <genesis.hiciano@ms223.org>
2016-05-24 09:26:16 -0400

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,17 @@
+class color(object):
+    """define a color"""
+
+Black=color()
+Black.r=26
+Black.G=8
+Black.B=8
+Aqua_marine = color()
+Aqua_marine.r=139
+Aqua_marine.G=226
+Aqua_marine.B=247
+Navy_Blue  = color()
+Navy_Blue.r=12
+Navy_Blue.G=12
+Navy_Blue.B=137
+
+print (Navy_Blue.r+Black.r)/2
